b513e3ebbe7954ecf5db5c6c1efff82f989f7493
aswamin6 <aswamin6@asu.edu>
2014-09-29 09:39:11 -0700

    Comments added to code

--- zap.js
+++ zap.js
@@ -1,21 +1,25 @@
-var pdt_arr = [];
+var pdt_arr = []; //product array which stores all the products with price lesser than desired total 
+var page = 1;	//page number to navigate through search results
+var giftnum;	//desired # of products
+var total;		//desired total
 var flag = 0;
-var page = 1;
-var giftnum;
-var total;
+
+/* 
+function to  generate combination of products based on desired total and number of products
+*/
 
 function combine(a, min, subsets_total) 
 {
     var all = [];
     for (var i = min; i < a.length; i++) 
 	{
-		console.log("herell");
         fn(i, a, [], all, subsets_total);
     }
-	
-    //all.push(a);
     return all;
 }
+
+// Function to generate subsets from selected products 
+
 
 function fn(n, src, got, all, subsets_total) 
 {	
@@ -31,9 +35,9 @@ function fn(n, src, got, all, subsets_total)
 	for (var j = 0; j < src.length; j++) 
 	{	
 		var curr_price = Number(src[j]["price"].replace("$", ""));
-		subsets_total += curr_price;
+		subsets_total += curr_price;	
 		if(subsets_total <= total){
-			fn(n - 1, src.slice(j + 1), got.concat([src[j]]), all, subsets_total);
+			fn(n - 1, src.slice(j + 1), got.concat([src[j]]), all, subsets_total);		//different products with lesser difference is cost are considered
 		} else {
 			subsets_total = 0;
 			break;
@@ -42,6 +46,7 @@ function fn(n, src, got, all, subsets_total)
 	return;
 }
 
+/*init funtion to get input from the user */
 
 function init()
 {
@@ -50,6 +55,8 @@ function init()
 	$("#myid").html('');
 	getdata();
 }
+
+// function to get the data from the Zappos API page and display the selected combination of product subsets with limited fields 
 
 function getdata(){
 	if(!flag){
@@ -70,9 +77,15 @@ function getdata(){
 	}
 }
 
+/* function to retrieve the JSON objects from the zappos product API 
+   Step 1: Get the API product data from the URL page by page
+   Step 2: Convert the JSON objects to an array list (array of arrays)
+   Step 3: Now filter only the productID and the price from the JSON objects 
+   Step 4: Push the elements of the array list (source:JSON object) if their price is lesser than the input desired total 
+*/
+
 function getjson(page){
 	
-		
 	var surl = "http://api.zappos.com/Search?key=52ddafbe3ee659bad97fcce7c53592916a6bfd73&term=&sort={%22price%22:%22asc%22}&page="+page;
 	$.ajax({
 		url: surl,
@@ -107,8 +120,7 @@ function getjson(page){
 					}
 				}
 			}
-			getdata();
-			
+			getdata();			
 		}
 	});
 }
